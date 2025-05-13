c6694d45935eae6a4b905e1a69ee9e44f4d121e6
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-05-13 22:02:21 +0530

    adding cart.jsx

---
+++ src/components/Cart.jsx
@@ -0,0 +1,68 @@
+import React, { useContext, useEffect } from 'react';
+import './Cart.css'; // if you created a separate file
+import { CartContext } from '../contex/CartContex';
+import { UserContex } from '../contex/userContex';
+import axios from 'axios';
+
+function Cart() {
+  const { cartItems, setCartItems} = useContext(CartContext);
+  const {user} = useContext(UserContex);
+
+  let totalAmount = cartItems.reduce((total , item)=>
+    total + item.item_price * item.item_qty, 0
+  )
+
+  function checkOutHandler(){
+    cartItems.map(async (item)=>{
+      // await axios.post("http://localhost:5000/api/postMyOrder", item);
+      console.log(item)
+    })
+  }
+
+  async function getCartItem(){
+    if(user){
+      const res = await axios.get(`http://localhost:5000/api/getCartData/${user._id}`);
+      setCartItems(res.data.data);
+    }
+  }
+
+  useEffect(()=>{
+    getCartItem();
+  },[])
+
+  return (
+    <div className="cart-container">
+      <h2>Your Cart</h2>
+      {cartItems.length === 0 ? 
+        (
+          <p className="empty-cart">Your cart is empty.</p>
+        ) : 
+        (
+          cartItems.map((item) => (
+            <div key={item._id} className="cart-item">
+              <img src={item.item_image} alt={item.item_name} />
+              <div className="cart-item-details">
+                <h3>{item.item_name}</h3>
+                <div className="price">${item.item_price}</div>
+                <div className="quantity-container">
+                  <div>Quantity: </div>
+                  <button>+</button>
+                  <div className="quantity">{item.item_qty}</div>
+                  <button>-</button>
+                </div>
+              </div>
+              <button className='cart-item-button'>Remove</button>
+            </div>
+          ))
+        )
+      }
+
+      <div className='checkoutContainer'>
+        <div>Total amount : {totalAmount.toFixed(2)}</div>
+        <button onClick={checkOutHandler}>CheckOut</button>
+      </div>
+    </div>
+  );
+}
+
+export default Cart;

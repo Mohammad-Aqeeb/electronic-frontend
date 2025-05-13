0aae28dfd09a807c6abdcc9f18623a3d51c12130
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-05-13 21:58:24 +0530

    adding login.jsx

---
+++ src/pages/Login.jsx
@@ -0,0 +1,82 @@
+import React, { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Login.css';
+import { UserContex } from '../contex/userContex';
+import toast from 'react-hot-toast';
+import axios from 'axios';
+
+function Login() {
+
+  const navigate = useNavigate();
+  const [ formData, setFormData ] = useState({email : "" , password : "", rememberMe : false});
+  const {setUser}  = useContext(UserContex);
+
+  function changeHandler(event){
+    setFormData((prev)=>{
+        return({
+            ...prev,
+            [event.target.name] : event.target.type ==="checkbox" ? event.target.checked : event.target.value
+        })
+    })
+  }
+
+  async function handleSubmit(event){
+    event.preventDefault();   
+    const res = await axios.post("http://localhost:5000/api/userLogin", formData);
+    setUser(res.data.data);
+    toast.success("User Login success");
+    navigate("/");
+  }
+
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <h2>Sign in to your account</h2>
+        <form onSubmit={handleSubmit}>
+
+          <label htmlFor="email">Email address</label>
+          <input 
+            type="email" 
+            id="email" 
+            className='loginInput'
+            placeholder="example@mail.com"
+            name="email"
+            onChange={changeHandler}
+            required 
+          />
+
+          <label htmlFor="password">Password</label>
+          <input 
+            type="password" 
+            id="password"
+            className='loginInput'
+            placeholder="Enter your password"
+            name='password'
+            onChange={changeHandler}
+            required 
+          />
+
+          <div className='rememberMeContainer'>
+            <input 
+                type='checkbox' 
+                id='remeberMe'
+                name='rememberMe'
+                onChange={changeHandler}
+            />
+            <label htmlFor='remeberMe' className='remember-label'>Remember Me</label>
+          </div>
+
+          <button type="submit">Login</button>
+        </form>
+
+        <p className="signup-link">
+          Don't have an account? <span onClick={() => navigate('/signup')}>Sign up</span>
+        </p>
+
+      </div>
+    </div>
+  );
+}
+
+export default Login;
+

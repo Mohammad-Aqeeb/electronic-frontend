788bf5501c6ca3d2833f7a5bd216462e2c79598c
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-05-13 21:56:52 +0530

    adding index.js

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Cart from './components/Cart';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import './App.css';
+import CreateProduct from './components/CreateProduct';
+import SellerDashboard from './components/sellerDashboard';
+
+function App() {
+  return (
+    <div className='Container'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/createProduct" element={<CreateProduct/>} />
+        <Route path='/sellerDashboard' element={<SellerDashboard/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+

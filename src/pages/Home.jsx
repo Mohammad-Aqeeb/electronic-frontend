d3b7354d9417a0ece0ec52149e0145e5a39e6fa8
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-05-16 20:44:20 +0530

    change in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,35 @@
-import React, { useContext, useState } from 'react';
+import { useContext, useState } from 'react';
 import './Home.css';
 import Header from "../components/Header";
 import ProductCard from '../components/ProductCard';
 import api from '../api';
-
-// { id: 1, name: "Wireless Headphones", price: 59.99, image: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6296/6296111_rd.jpg"},
-// { id: 2, name: "Smart Watch", price: 89.99, image: "" },
-// { id: 3, name: "Bluetooth Speaker", price: 39.99, image: "https://static3.srcdn.com/wordpress/wp-content/uploads/2020/10/81g26BxrTAL.-AC-SL1500-.jpg"},
-// { id: 4, name: "Gaming Mouse", price: 29.99, image: "https://th.bing.com/th/id/OPAC.PhfxGyB7R28LmA474C474?w=592&h=550&o=5&dpr=1.3&pid=21.1" },
+import toast from 'react-hot-toast';
+import { CartContext } from '../contex/CartContex';
+import Footer from '../components/Footer';
 
 function Home() {
   const [products, setProducts] = useState([]);
+  const {setCartItems} = useContext(CartContext);
+  const user = JSON.parse(localStorage.getItem("user"));
 
   async function getData(){
-    const res = await api.get("/itemGet");
-    setProducts(res.data.data);
+    try{
+      let res = await api.get("/itemGet");
+      setProducts(res.data.data);
+      if(user){
+      res = await api.get(`/getCartData/${user._id}`);
+      setCartItems(res.data.data);
+      }
+    }
+    catch(error){
+      console.log(error);
+      toast.error("Failed to fetch items");
+    }
   }
 
   useState(()=>{
     getData();
-  },[])
+  },[]);
 
   return (
     <div>
@@ -36,6 +46,7 @@ function Home() {
           }
         </div>
       </div>
+      <Footer/>
     </div>
   );
 }

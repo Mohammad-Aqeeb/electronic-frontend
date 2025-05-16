7c77dbe123fc88edf66d3c7956a506d8f54c4fdc
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-05-16 20:44:44 +0530

    change in login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Login.css';
-import { UserContex } from '../contex/userContex';
 import toast from 'react-hot-toast';
 import api from '../api';
 
@@ -31,7 +30,8 @@ function Login() {
       navigate("/");
     }
     catch(error){
-      toast.error(error);
+      console.log(error)
+      toast.error(error.response.data.message);
     }
   }
 

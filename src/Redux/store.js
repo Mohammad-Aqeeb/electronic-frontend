e0e85b42b2d77bd8a2d5631f8005c0cb22aa63a9
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-05-25 12:10:37 +0530

    adding store for slice

---
+++ src/Redux/store.js
@@ -0,0 +1,24 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './slice/cartSlice';
+// import signupReducer from './slice/signupSlice';
+// import loginReducer from './slice/loginSlice';
+import TrackSellerOrderReducer from './slice/TrackSellerOederSlice';
+import TrackMyOrderReducer from './slice/TrackMyOrderSlice';
+import sellerDashboardReducer from './slice/sellerDashboardSlice';
+import myOrderReducer from './slice/myOrderSlice';
+import adminDashbordReducer from './slice/adminDashBoardSlice';
+import productReducer from "./slice/productSlice"
+
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+    // signup: signupReducer,
+    // login: loginReducer,
+    sellerOrder : TrackSellerOrderReducer,
+    trackmyOrder : TrackMyOrderReducer,
+    sellerDashboard : sellerDashboardReducer,
+    myOrder : myOrderReducer,
+    adminDashbord : adminDashbordReducer,
+    product  : productReducer
+  }
+});

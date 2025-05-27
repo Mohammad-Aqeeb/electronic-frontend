45f0cb2f6768999b3d8ac03faf377db305805f5d
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-05-27 18:10:51 +0530

    change in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './Home.css';
 import Header from "../components/Header";
 import ProductCard from '../components/ProductCard';

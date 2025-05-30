c5b845559677ee45035ae531dcf8affa9b147004
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-05-30 15:34:36 +0530

    change in app component

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import NetworkWatcher from './components/NetworkWatcher';
 import UserProfile from './components/userProfile';
 import NewOrder from './components/newOrder';
 import MyProducts from './components/MyProducts';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -28,19 +29,49 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/user" element={<UserProfile />} />
-        <Route path="/cart" element={<Cart/>} />
+        {/* <Route path="/cart" element={<Cart/>} />
         <Route path="/MyOrder" element={<MyOrder/>} />
-        <Route path="/trackOrder/:id" element={<TrackMyOrder />} />
-        <Route path="/sellerDashboard" element={<SellerDashboard />}>
+        <Route path="/trackOrder/:id" element={<TrackMyOrder />} /> */}
+        
+        {/* <Route path="/sellerDashboard" element={<SellerDashboard />}>
           <Route index element={<NewOrder/>} />             
           <Route path='newOrder' element={<NewOrder/>} />
           <Route path='myProducts' element={<MyProducts/>} />
           <Route path="createProduct" element={<CreateProduct/>} />
-        </Route>
+        </Route> */}
         <Route path="/TrackSellerOrder/:id" element={<TrackSellerOrder/>} />
+
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/no-internet" element={<NoInternet />} />
         <Route path="*" element={<NotFoundPage/>} />
+
+        {/* User-only routes */}
+          <Route path="/cart" element={
+            <ProtectedRoute allowedRoles={['User']}>
+              <Cart />
+            </ProtectedRoute>
+          } />
+          <Route path="/MyOrder" element={
+            <ProtectedRoute allowedRoles={['User']}>
+              <MyOrder />
+            </ProtectedRoute>
+          } />
+          <Route path="/trackOrder/:id" element={
+            <ProtectedRoute allowedRoles={['User']}>
+              <TrackMyOrder />
+            </ProtectedRoute>
+          } />
+          
+          <Route path="/sellerDashboard" element={
+              <ProtectedRoute allowedRoles={['Seller']}>
+                <SellerDashboard />
+              </ProtectedRoute>}
+          >
+            <Route index element={<NewOrder/>} />             
+            <Route path='newOrder' element={<NewOrder/>} />
+            <Route path='myProducts' element={<MyProducts/>} />
+          <Route path="createProduct" element={<CreateProduct/>} />
+        </Route>
       </Routes>
     </div>
   );

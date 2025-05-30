fd835de5af9bd92272b22f53487ada4d235fbec6
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-05-30 12:56:29 +0530

    change in authcontex

--- src/contex/AuthContex.jsx
+++ src/contex/AuthContex.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { setLogout } from '../logoutHandler';
 
